f0eb64df70266b761b5bf2ad5a8fe9a161a450a7
 z <z@z.local>
2018-03-14 13:03:45 +0800

    add ss install

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 ````text
 
-wget -qO- https://raw.githubusercontent.com/v3u3i87/ops/master/gitInstall.sh | /usr/bin/bash && source /etc/bashrc && git --version
+wget -qO- https://raw.githubusercontent.com/v3u3i87/ops/master/gitInstall.sh | bash && source /etc/bashrc && git --version
+
+````
+
+
+
+## 安装SS, 仅限cenots7 
+
+````text
+
+wget -qO- https://raw.githubusercontent.com/v3u3i87/ops/master/ssInstall.sh | bash
 
 ````
---
+++ ssInstall.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd /usr/local/src
+
+yum -y update
+
+wget -qO- https://raw.githubusercontent.com/v3u3i87/ops/master/gitInstall.sh | bash && source /etc/bashrc && git --version
+
+yum install python-setuptools && easy_install pip
+pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+pip install shadowsocks
+
+
+#读取外网 IP: curl -s ipecho.net/plain;echo
+
+#多用户配置
+touch /etc/shadowsocks.json
+
+tee /etc/shadowsocks.json <<-'EOF'
+{
+ "server":"0.0.0.0",
+ "local_address": "127.0.0.1",
+ "local_port":1080,
+  "port_password": {
+     "9933": "9933##111",
+ },
+ "timeout":300,
+ "method":"aes-256-cfb",
+ "fast_open": false
+}
+EOF
+
+#设置自启
+echo -e "/usr/bin/ssserver  -c /etc/shadowsocks.json -d start \n" >> /etc/rc.local
+
+chmod +x /etc/rc.d/rc.local
+
+ssserver -c /etc/shadowsocks.json -d start
